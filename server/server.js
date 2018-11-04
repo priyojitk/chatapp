269c691a2d8dbcf7d579d3ecf72037ef571b863b
Priyojit Kharibam <priyojitkharibam@gmail.com>
2018-11-04 22:05:46 +0530

    added newMessage

--- server/server.js
+++ server/server.js
@@ -1,10 +1,35 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '/../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port, () =>{
-    console.log(`Server is up on port ${port}`);  
-} ); 
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    
+    socket.emit('newMessage', {
+        from: 'John',
+        text: 'Hey See you then',
+        createAt: 12345
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected from server');
+
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});

be09f25411c31053149b3d8d134a7cec95a3a7e7
Priyojit Kharibam <priyojitkharibam@gmail.com>
2018-11-04 22:29:09 +0530

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from: 'Priyo',
-        text: 'Hey It works'
-    });
-
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -13,15 +13,17 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('new user connected');
-    
-    socket.emit('newMessage', {
-        from: 'John',
-        text: 'Hey See you then',
-        createAt: 12345
-    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date()
+
+        });
+
     });
 
     socket.on('disconnect', () => {

f270173ebc58ad2d8974c02a6fe5e95005c49754
Priyojit Kharibam <priyojitkharibam@gmail.com>
2018-11-05 21:55:53 +0530

    added form

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {
+    generateMessage
+} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,16 +17,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.on('createMessage', (message) => {
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user Joined'));
+
+
+
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
 
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createAt: new Date()
-
-        });
-
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server');
     });
 
     socket.on('disconnect', () => {

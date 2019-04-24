40fc94f4e7113138e78ab06f4f09e4e332765d17
Priyojit Kharibam <priyojitkharibam@gmail.com>
2019-04-24 20:34:30 +0530

    final

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageheight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + lastMessageheight + newMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -18,7 +34,7 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
-
+    scrollToBottom();
     // 
     // console.log('New Message', message);
     // var li = jQuery('<li></li>');
@@ -36,9 +52,9 @@ socket.on('newLocationMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 
-    
     // var li = jQuery('<li></li>');
     // var a = jQuery('<a target="_blank">My current location</a>')
     // li.text(`${message.from}: ${formattedTime} `);
